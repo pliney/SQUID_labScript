eefacaf8a38493189cfdec094a0bcf95c974f3a1
pliney <mchambl3@msudenver.edu>
2017-07-04 12:55:53 -0600

    Conversion working, need to work on ui

--- kalamari_script.py
+++ kalamari_script.py
@@ -48,10 +48,11 @@ def add_headers(matrix, bias_step, headerstring='I_Flux, I_Bias, Voltage, dV/dI 
 def format_data(data_matrix, num_steps, num_bias_steps=3):
     """
 
-    :param data_matrix:
-    :param num_steps:
-    :param num_bias:
-    :return:
+    :param data_matrix: A 2d array containing the data values from noise file
+    :param num_steps: Number of flux steps
+    :param num_bias: Number of bias steps
+    :return: A formatted 2d numpy array with the data from each bias step seperated into seperate columns, headers have
+            also been added to the top of each column.
     """
     #Seperates each line into seperate data entries and removes whitespace
     split_matrix = [line.strip().replace(' ', '').split(',') for line in data_matrix]
@@ -76,31 +77,33 @@ def format_data(data_matrix, num_steps, num_bias_steps=3):
     return formatted_data
 
 
-def get_data_dir():
-    return ''
+def get_data_filename():
+    return 'W34R4C7'
 
 
 def get_save_dir():
-    return ''
+    return 'test2.csv'
+
+
+def get_num_flux_steps():
+    return 60
 
 
 def save_csv(filename, processed_data):
-    np.savetxt(filename, processed_data, fmt='%s', delimiter=',')
-
-
-def get_num_steps():
-    pass
+    np.savetxt(filename, processed_data, fmt='%s', delimiter=', ')
 
 
 def main():
-    get_data_dir()
-    get_save_dir()
-    num_steps = get_num_steps()
+    noise_filename=get_data_filename()
+    save_dir = get_save_dir()
+    num_steps = get_num_flux_steps()
+    num_bias_steps = 3
+
+    file_data = get_data_from_file(noise_filename)
+    num_steps = get_num_flux_steps()
+    processed_data = format_data(file_data, num_steps)
+    save_csv(save_dir, processed_data)
 
 
 if __name__ == "__main__":
-    num_bias_steps = 3
-
-    file_data = get_data_from_file("W34R4C7")
-    processed_data = format_data(file_data, 60)
-    save_csv('test.csv',processed_data, num_bias_steps)
+    main()
